0dbe96180654f22d66b89351c231d8e694feeba4
Lalit0905 <109729941+Lalit0905@users.noreply.github.com>
2025-11-01 18:09:30 +0530

    Add files via upload

---
+++ regression.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+data = pd.read_csv('slr.csv')
+data.describe()
+print(data.describe())
+y = data['GPA']
+x1 = data['SAT']
+plt.scatter(x1,y)
+plt.xlabel('SAT',fontsize=20)
+plt.ylabel('GPA',fontsize=20)
+plt.show()
+x = sm.add_constant(x1)
+results = sm.OLS(y,x).fit()
+print(results.summary())
+plt.scatter(x1,y)
+yhat = 0.0017*x1 + 0.275
+fig = plt.plot(x1,yhat,lw=2,c='orange', label = 'regression line')
+plt.xlabel('SAT',fontsize=20)
+plt.ylabel('GPA',fontsize=20)
+plt.show()
+
+
+
+
